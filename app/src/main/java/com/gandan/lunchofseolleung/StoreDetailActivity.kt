5c34dea3c77337a0a156b322b33ed3ea849568e3
pleasantlife <pleasantlife@gmail.com>
2019-09-29 00:12:40 +0900

    190929 Navermap added

--- app/src/main/java/com/gandan/lunchofseolleung/StoreDetailActivity.kt
+++ app/src/main/java/com/gandan/lunchofseolleung/StoreDetailActivity.kt
@@ -3,9 +3,18 @@ package com.gandan.lunchofseolleung
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
 import com.google.firebase.firestore.FirebaseFirestore
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
 
-class StoreDetailActivity : AppCompatActivity() {
+class StoreDetailActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var lat : Double = 0.0
+    private var lng : Double = 0.0
+    private lateinit var naverMap : NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,12 +22,33 @@ class StoreDetailActivity : AppCompatActivity() {
 
         var db = FirebaseFirestore.getInstance()
 
+        var storeMapFragment : MapView = findViewById<View>(R.id.storeMapFragment) as MapView
+        storeMapFragment.getMapAsync(this)
+
         db.collection("storelist").whereEqualTo("storeName", intent.getStringExtra("storeName"))
             .get().addOnSuccessListener {
                 query ->
                 for(document in query){
                     Log.e("document", "${document.get("storeName")}")
+                    lat = document.get("lat") as Double
+                    lng = document.get("lng") as Double
+                    if(lat != 0.0 && lng != 0.0)
+                    onMapReady(naverMap)
                 }
             }
+
+
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.let {
+            it.moveCamera(CameraUpdate.scrollTo(LatLng(lat, lng)))
+        }
+        val marker = Marker()
+        marker.let {
+            it.position = LatLng(lat, lng)
+            it.map = naverMap
+        }
     }
 }
